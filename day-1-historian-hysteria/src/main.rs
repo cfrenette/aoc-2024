f024e5ee42bf6a3210fa151f0d244832761876e9
Cory Frenette <cory@frenette.dev>
2024-12-07 15:49:07 -0500

    clean up day 1

--- day-1-historian-hysteria/src/main.rs
+++ day-1-historian-hysteria/src/main.rs
@@ -2,7 +2,6 @@ use aoc_2024::read_input_by_line;
 use std::collections::HashMap;
 
 fn main() {
-    // TODO: optimize solution
     if let Ok(lines) = read_input_by_line("input") {
         let lists = lines.map(|line_result| {
             let line = line_result.unwrap();
@@ -20,6 +19,21 @@ fn main() {
     }
 }
 
+fn part_one(left_list: &mut Vec<usize>, right_list: &mut Vec<usize>) {
+    left_list.sort();
+    right_list.sort();
+    let accum = |acc, e| {
+        let (left, right) = e;
+        if left <= right {
+            acc + (right - left)
+        } else {
+            acc + (left - right)
+        }
+    };
+    let zipped = left_list.iter().zip(right_list.iter());
+    println!("Total distance: {}", zipped.fold(0, accum));
+}
+
 fn part_two(left_list: &Vec<usize>, right_list: &Vec<usize>) {
     // populate multiplicities
     let mut multiplicity_map = HashMap::new();
@@ -35,20 +49,5 @@ fn part_two(left_list: &Vec<usize>, right_list: &Vec<usize>) {
     let result = left_list.iter().fold(0, |acc, e| {
         acc + (e * multiplicity_map.get(&e).unwrap_or_else(|| &0))
     });
-    println!("Part Two Result: {}", result);
-}
-
-fn part_one(left_list: &mut Vec<usize>, right_list: &mut Vec<usize>) {
-    left_list.sort();
-    right_list.sort();
-    let accum = |acc, e| {
-        let (left, right) = e;
-        if left <= right {
-            acc + (right - left)
-        } else {
-            acc + (left - right)
-        }
-    };
-    let zipped = left_list.iter().zip(right_list.iter());
-    println!("Part One Result: {}", zipped.fold(0, accum));
+    println!("Similarity score: {}", result);
 }

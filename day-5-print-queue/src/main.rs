9b521aee1f2fadbe0baf8717615cf1b038894a01
Cory Frenette <cory@frenette.dev>
2024-12-08 13:37:10 -0500

    day 5 complete (messy)

--- day-5-print-queue/src/main.rs
+++ day-5-print-queue/src/main.rs
@@ -8,11 +8,56 @@ use aoc_2024::read_input_by_line;
 
 fn main() {
     if let Ok(mut lines) = read_input_by_line("input") {
-        part_one(&mut lines);
+        let rules = load_rules(&mut lines);
+
+        let mut sum = 0;
+        let mut not_sorted_sum = 0;
+
+        // for each update validate the pages using loaded rules
+        while let Some(Ok(line)) = lines.next() {
+            // parse the page numbers from the update
+            let mut update = line
+                .split(',')
+                .map(|p| p.parse().unwrap())
+                .collect::<Vec<usize>>();
+
+            // hashset to hold pages that must not appear after
+            let mut disallowed = HashSet::new();
+            let mut is_valid = true;
+            for page in &update {
+                // a disallowed page was found, set is_valid false and
+                // go to next iteration
+                if disallowed.contains(&page) {
+                    is_valid = false;
+                    break;
+                }
+
+                // add disallowed pages from the rules to the set
+                if let Some(rules_pages) = rules.get(page) {
+                    for page in rules_pages {
+                        disallowed.insert(page);
+                    }
+                }
+            }
+
+            // if valid, add the middle page number to the sum
+            if is_valid {
+                sum += update[update.len() / 2];
+            } else {
+                sort(&mut update, &rules);
+                not_sorted_sum += update[update.len() / 2];
+            }
+        }
+
+        println!("Sum of already sorted middle page numbers: {}", sum);
+        println!(
+            "Sum of not-sorted middle page numbers once sorted: {}",
+            not_sorted_sum
+        )
     }
 }
 
-fn part_one(lines: &mut Lines<BufReader<File>>) {
+fn load_rules(lines: &mut Lines<BufReader<File>>) -> HashMap<usize, Vec<usize>> {
     // rules map, key -> Vec of pages that must come before it
     let mut rules = HashMap::with_capacity(lines.size_hint().0);
 
@@ -37,40 +82,59 @@ fn part_one(lines: &mut Lines<BufReader<File>>) {
             .or_insert(Vec::from([val]));
     }
 
-    let mut sum = 0;
+    rules
+}
 
-    // for each update validate the pages using loaded rules
-    while let Some(Ok(line)) = lines.next() {
-        // parse the page numbers from the update
-        let update = line
-            .split(',')
-            .map(|p| p.parse().unwrap())
-            .collect::<Vec<usize>>();
+fn sort(update: &mut Vec<usize>, rules: &HashMap<usize, Vec<usize>>) {
+    // topological sort (backwards edition)
+    // TODO: clean this up
 
-        // hashset to hold pages that must not appear after
-        let mut disallowed = HashSet::new();
-        let mut is_valid = true;
-        for page in &update {
-            // a disallowed page was found, set is_valid false and
-            // go to next iteration
-            if disallowed.contains(&page) {
-                is_valid = false;
-                break;
-            }
+    // create an adjacency list
+    let mut adj: Vec<Vec<usize>> = vec![Vec::new(); update.len()];
+    let mut pages_in_update = HashMap::with_capacity(update.len());
 
-            // add disallowed pages from the rules to the set
-            if let Some(rules_pages) = rules.get(page) {
-                for page in rules_pages {
-                    disallowed.insert(page);
+    // map each value in update to its index
+    for (j, page) in update.iter().enumerate() {
+        pages_in_update.insert(page, j);
+    }
+
+    // only add edges from rules for pages that are in the update
+    for (i, page) in update.iter().enumerate() {
+        if let Some(neighbors) = rules.get(page) {
+            for neighbor in neighbors {
+                if let Some(j) = pages_in_update.get(neighbor) {
+                    adj[i].push(*j);
                 }
             }
         }
-
-        // if valid, add the middle page number to the sum
-        if is_valid {
-            sum += update[update.len() / 2];
-        }
     }
 
-    println!("Sum of middle page numbers: {}", sum);
+    // actually do the topo sort
+    let mut visited = vec![false; update.len()];
+    let mut sorted = Vec::with_capacity(update.len());
+    for i in 0..update.len() {
+        if !visited[i] {
+            dfs(update, &adj, &mut visited, &mut sorted, i);
+        }
+    }
+    for i in 0..sorted.len() {
+        update[i] = sorted[i];
+    }
+}
+
+fn dfs(
+    update: &mut Vec<usize>,
+    adj: &Vec<Vec<usize>>,
+    visited: &mut [bool],
+    sorted: &mut Vec<usize>,
+    i: usize,
+) {
+    visited[i] = true;
+    let neighbors = &adj[i];
+    for neighbor in neighbors {
+        if !visited[*neighbor] {
+            dfs(update, adj, visited, sorted, *neighbor)
+        }
+    }
+    sorted.push(update[i]);
 }
